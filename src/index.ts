ea7ffdf69314fb0a94cd2ef48675ce4e4cceb06a
Anxhul10 <anxhul10@gmail.com>
2025-11-27 23:08:28 +0530

    fix: unknown file ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ import { Client } from 'cyborgdb';
 import { config } from 'dotenv';
 
 config();
-const client = new Client('http://localhost:8000', process.env.CYBORGDB_API_KEY.)
+
+const API = process.env.CYBORGDB_API_KEY;
+
+const localClient = new Client({baseUrl: 'http://localhost:8000'});

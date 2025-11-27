8d4953be858f3691ddcd2d64ab2ff78fda83fef4
Anxhul10 <anxhul10@gmail.com>
2025-11-27 22:34:27 +0530

    fix: env

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Client } from 'cyborgdb';
+import 'dotenv/config'
 
 const client = new Client('http://localhost:8000', process.env.CYBORGDB_API_KEY)
-
-const localClient = new Client('http://localhost:8000');

ac00afb2e98c6a1e3367185451a7db17f471fc05
Anxhul10 <anxhul10@gmail.com>
2025-11-28 00:28:35 +0530

    chore: add lint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import { Client } from 'cyborgdb';
+// import { Client } from 'cyborgdb';
 import { config } from 'dotenv';
-
+//*eslitnt 
 config();
 
-const API = process.env.CYBORGDB_API_KEY;
+// const API = process.env.CYBORGDB_API_KEY;
 
-const localClient = new Client({baseUrl: 'http://localhost:8000'});
-
+// const localClient = new Client({baseUrl: 'http://localhost:8000'});

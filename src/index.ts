ced616a52f43515191e6035cc875b119ffc899b7
Anxhul10 <anxhul10@gmail.com>
2025-11-29 01:28:46 +0530

    chore: add supabase

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-// import { Client } from 'cyborgdb';
-import { config } from 'dotenv';
-config();
+import { createClient } from '@supabase/supabase-js'
+import * as dotenv from 'dotenv'
 
-// const API = process.env.CYBORGDB_API_KEY;
+dotenv.config()
+const supabaseUrl: string = 'https://hvhtnhzwubhmmricdpxw.supabase.co';
+const supabaseKey: string = process.env.supabaseKey || 'no key';
 
-// const localClient = new Client({baseUrl: 'http://localhost:8000'});
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const { data, error } = await supabase
+  .from('countries')
+  .insert({ id: 2, name: 'Mordor' })
+  .select()
+
+console.log(data);

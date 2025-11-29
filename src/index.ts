d6cb1668e0f84156c9be53081e17bb8a43d9ab84
Anxhul10 <anxhul10@gmail.com>
2025-11-30 01:34:52 +0530

    chore: add prettier configs and apply

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-import * as dotenv from 'dotenv';
-import { Client, type HealthResponse } from 'cyborgdb';
-import { CreateIndex } from './utils/CreateIndex.ts';
+import * as dotenv from "dotenv";
+import { Client, type HealthResponse } from "cyborgdb";
+import { CreateIndex } from "./utils/CreateIndex.ts";
 
-dotenv.config();
+// dotenv.config();
 
+// const localClient = new Client({ baseUrl: 'http://localhost:8000', apiKey: process.env.CYBORGDB_API_KEY });
 
-const localClient = new Client({ baseUrl: 'http://localhost:8000', apiKey: process.env.CYBORGDB_API_KEY });
-
-try {
-    const health: HealthResponse = await localClient.getHealth();
-    console.log('Service health status:', health);
-    console.log('Status:', health.status);
-} catch (error: any) {
-    console.error('Health check failed:', error.message);
-}
+// try {
+//     const health: HealthResponse = await localClient.getHealth();
+//     console.log('Service health status:', health);
+//     console.log('Status:', health.status);
+// } catch (error: any) {
+//     console.error('Health check failed:', error.message);
+// }
 
 CreateIndex();
--- src/supabase/sendData.ts
+++ src/supabase/sendData.ts
@@ -1,21 +1,19 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl: string = 'https://hvhtnhzwubhmmricdpxw.supabase.co';
-const supabaseKey: string = process.env.supabaseKey || 'no key';
+const supabaseUrl: string = "https://hvhtnhzwubhmmricdpxw.supabase.co";
+const supabaseKey: string = process.env.supabaseKey || "no key";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-
 export async function sendData() {
-    const { data, error } = await supabase
-    .from('countries')
-    .insert({ id: 2, name: 'Mordor' })
+  const { data, error } = await supabase
+    .from("countries")
+    .insert({ id: 2, name: "Mordor" })
     .select();
 
-    if (error ) {
-        console.log(error);
-    }
-    else {
-        console.log(data);
-    }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 }

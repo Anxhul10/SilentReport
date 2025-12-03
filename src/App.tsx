1392d0208ea85aafb2d29bb6d8fd59fae1fcdea6
Anxhul10 <anxhul10@gmail.com>
2025-12-03 22:45:51 +0530

    fix: prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { NavBarInitial, NavBar } from './components/ui/navBar';
+import { NavBarInitial, NavBar } from "./components/ui/navBar";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
       <NavBarInitial></NavBarInitial>
-      <div>
-        hii
-      </div>
+      <div>hii</div>
     </>
   );
 }
-
 
 export default App;
--- src/components/ui/navBar.tsx
+++ src/components/ui/navBar.tsx
@@ -1,29 +1,47 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 interface NavBarProps {
-    accountName: string
+  accountName: string;
 }
 
 export function NavBarInitial() {
-  return <>
-    <div className="navBar">
-      <Button variant="ghost">SilentReport</Button>
-      <div>
-        <Button variant="ghost" onClick={()=>{console.log('clicked')}}>Login</Button>
-        <Button variant="ghost">Signup</Button>
-      </div>
-    </div>
-  </>
-}
-
-export function NavBar({accountName}: NavBarProps) {
-    return <>
-        <div className="navBar">
+  return (
+    <>
+      <div className="navBar">
         <Button variant="ghost">SilentReport</Button>
         <div>
-            <Button variant="ghost" onClick={()=>{console.log('clicked')}}>{accountName}</Button>
-            <Button variant="ghost">Logout</Button>
+          <Button
+            variant="ghost"
+            onClick={() => {
+              console.log("clicked");
+            }}
+          >
+            Login
+          </Button>
+          <Button variant="ghost">Signup</Button>
         </div>
-        </div>
+      </div>
     </>
+  );
+}
+
+export function NavBar({ accountName }: NavBarProps) {
+  return (
+    <>
+      <div className="navBar">
+        <Button variant="ghost">SilentReport</Button>
+        <div>
+          <Button
+            variant="ghost"
+            onClick={() => {
+              console.log("clicked");
+            }}
+          >
+            {accountName}
+          </Button>
+          <Button variant="ghost">Logout</Button>
+        </div>
+      </div>
+    </>
+  );
 }

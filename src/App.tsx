89506d796618e07e04d73c69ddbead20fbf6b192
Anxhul10 <anxhul10@gmail.com>
2025-12-06 00:02:29 +0530

    fix: formating and specify yarn version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { NavBarInitial } from "./components/custom/navBar";
-import { SignupForm } from './components/signup-form';
+import { SignupForm } from "./components/signup-form";
 
 function App() {
   return (
@@ -11,10 +11,12 @@ function App() {
 }
 
 function SignUp() {
-  return <>
-    <div className="signupParent">
-        <SignupForm className="signupChild"/>
-    </div>
-  </>
+  return (
+    <>
+      <div className="signupParent">
+        <SignupForm className="signupChild" />
+      </div>
+    </>
+  );
 }
 export default App;

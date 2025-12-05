a85713dc5e52e3bdc087d3526d3dd1ba26fc96c5
Anxhul10 <anxhul10@gmail.com>
2025-12-06 00:17:02 +0530

    fix: format

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
 import "./App.css";
 import { NavBarInitial } from "./components/custom/navBar";
-import { SignupForm } from './components/signup-form';
+import { SignupForm } from "./components/signup-form";
 
 function App() {
   return (
     <>
       <NavBarInitial></NavBarInitial>
+      <SignUp></SignUp>
     </>
   );
 }
 
 function SignUp() {
-  return <>
-    <div className="signupParent">
-        <SignupForm className="signupChild"/>
-    </div>
-  </>
+  return (
+    <>
+      <div className="signupParent">
+        <SignupForm className="signupChild" />
+      </div>
+    </>
+  );
 }
 export default App;

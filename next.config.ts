28ecc8468440eb21958660478c0e97c59fee176d
Anxhul10 <anxhul10@gmail.com>
2025-12-09 19:30:00 +0530

    chore: disable dev indicator

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   crossOrigin: "anonymous",
+  devIndicators: false,
 };
 
 export default nextConfig;

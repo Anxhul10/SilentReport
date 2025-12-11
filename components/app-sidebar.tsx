e915f561c8b0cda3d751a85ab63ab916e4570d93
Anxhul10 <anxhul10@gmail.com>
2025-12-11 22:32:19 +0530

    chore: remove noise

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -14,9 +14,9 @@ import {
   IconReport,
   IconSearch,
   IconSettings,
+  IconPencilPlus,
   IconUsers,
 } from "@tabler/icons-react";
-
 import { NavDocuments } from "@/components/nav-documents";
 import { NavMain } from "@/components/nav-main";
 import { NavSecondary } from "@/components/nav-secondary";
@@ -44,72 +44,14 @@ const data = {
       icon: IconDashboard,
     },
     {
-      title: "Lifecycle",
+      title: "Feed",
       url: "#",
       icon: IconListDetails,
     },
     {
-      title: "Analytics",
+      title: "Search",
       url: "#",
-      icon: IconChartBar,
-    },
-    {
-      title: "Projects",
-      url: "#",
-      icon: IconFolder,
-    },
-    {
-      title: "Team",
-      url: "#",
-      icon: IconUsers,
-    },
-  ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: IconCamera,
-      isActive: true,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: IconFileDescription,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: IconFileAi,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
+      icon: IconSearch,
     },
   ],
   navSecondary: [
@@ -118,32 +60,17 @@ const data = {
       url: "#",
       icon: IconSettings,
     },
-    {
-      title: "Get Help",
-      url: "#",
-      icon: IconHelp,
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: IconSearch,
-    },
   ],
   documents: [
     {
-      name: "Data Library",
+      name: "Create Report",
       url: "#",
-      icon: IconDatabase,
+      icon: IconPencilPlus,
     },
     {
-      name: "Reports",
+      name: "View Reports",
       url: "#",
       icon: IconReport,
-    },
-    {
-      name: "Word Assistant",
-      url: "#",
-      icon: IconFileWord,
     },
   ],
 };

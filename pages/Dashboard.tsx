bd660f61742d35efe33ed4f1de53385b4c47d97c
Anxhul10 <anxhul10@gmail.com>
2025-12-12 19:46:08 +0530

    chore: use state for nav

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -10,7 +10,7 @@ import { useEffect, useState } from "react";
 import data from "./data.json";
 
 export default function Dashboard() {
-  const [onDashHit, setDashHit] = useState(false);
+  const [index, setIndex] = useState(0);
   const router = useRouter();
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -18,11 +18,77 @@ export default function Dashboard() {
       router.push("/Login");
     }
   });
-  if (onDashHit) {
+  if (index === 1) {
     return (
-      <>
-        <DashSide setDashHit={setDashHit}></DashSide>
-      </>
+      <div>
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar
+            variant="inset"
+            onDashboardParent={() => {
+              setIndex(0);
+            }}
+            onFeedParent={() => {
+              setIndex(1);
+            }}
+            onSearchParent={() => {
+              setIndex(2);
+            }}
+          />
+          <SidebarInset>
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  <div className="px-4 lg:px-6">feed...</div>
+                </div>
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </div>
+    );
+  } else if (index === 2) {
+    return (
+      <div>
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar
+            variant="inset"
+            onDashboardParent={() => {
+              setIndex(0);
+            }}
+            onFeedParent={() => {
+              setIndex(1);
+            }}
+            onSearchParent={() => {
+              setIndex(2);
+            }}
+          />
+          <SidebarInset>
+            <SiteHeader />
+            <div className="flex flex-1 flex-col">
+              <div className="@container/main flex flex-1 flex-col gap-2">
+                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                  <div className="px-4 lg:px-6">search...</div>
+                </div>
+              </div>
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
+      </div>
     );
   }
   return (
@@ -38,13 +104,13 @@ export default function Dashboard() {
         <AppSidebar
           variant="inset"
           onDashboardParent={() => {
-            setDashHit(true);
+            setIndex(0);
           }}
           onFeedParent={() => {
-            alert("feed");
+            setIndex(1);
           }}
           onSearchParent={() => {
-            alert("seach");
+            setIndex(2);
           }}
         />
         <SidebarInset>
@@ -63,45 +129,5 @@ export default function Dashboard() {
         </SidebarInset>
       </SidebarProvider>
     </div>
-  );
-}
-
-function DashSide({ setDashHit }: any) {
-  return (
-    <>
-      <div>
-        <SidebarProvider
-          style={
-            {
-              "--sidebar-width": "calc(var(--spacing) * 72)",
-              "--header-height": "calc(var(--spacing) * 12)",
-            } as React.CSSProperties
-          }
-        >
-          <AppSidebar
-            variant="inset"
-            onDashboardParent={() => {
-              setDashHit(false);
-            }}
-            onFeedParent={() => {
-              alert("feed");
-            }}
-            onSearchParent={() => {
-              alert("seach");
-            }}
-          />
-          <SidebarInset>
-            <SiteHeader />
-            <div className="flex flex-1 flex-col">
-              <div className="@container/main flex flex-1 flex-col gap-2">
-                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                  <div className="px-4 lg:px-6">dashboard wip ......</div>
-                </div>
-              </div>
-            </div>
-          </SidebarInset>
-        </SidebarProvider>
-      </div>
-    </>
   );
 }

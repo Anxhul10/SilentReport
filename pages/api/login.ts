0da53baddf950083c3bd301771e6677471905e84
Anxhul10 <anxhul10@gmail.com>
2025-12-14 00:52:46 +0530

    chore: set user id to local storage

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -5,6 +5,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 type ApiResponse = {
   status: number;
   token: string | undefined;
+  user_id: string | undefined;
 };
 
 export default async function login(
@@ -13,16 +14,18 @@ export default async function login(
 ) {
   const supabaseUrl = process.env.supabaseUrl || "no key";
   const supabaseKey = process.env.supabaseKey || "no key";
-  const data = await loginUser(
+  const { user, token } = await loginUser(
     supabaseUrl,
     supabaseKey,
     req.body.email,
     req.body.password,
   );
-  if (!data) {
+  if (!user) {
     // login invalid
-    return res.status(400).json({ status: 400, token: undefined });
+    return res
+      .status(400)
+      .json({ status: 400, token: undefined, user_id: undefined });
   }
   // login valid
-  return res.status(200).json({ status: 200, token: data.token });
+  return res.status(200).json({ status: 200, token: token, user_id: user.id });
 }

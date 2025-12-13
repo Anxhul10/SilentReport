7a8771ac216df9a1eb7e63599de7e447dfe2d8af
Anxhul10 <anxhul10@gmail.com>
2025-12-13 21:18:30 +0530

    feat: add ui

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -7,6 +7,17 @@ import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { PageLayout } from "../components/PageLayout";
+import {
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardContent,
+} from "../components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { InputGroup, InputGroupTextarea } from "@/components/ui/input-group";
+import { Label } from "@/components/ui/label";
 
 import data from "./data.json";
 
@@ -19,12 +30,18 @@ export default function Dashboard() {
       router.push("/Login");
     }
   });
+  // 0. Dashboard
+  // 1. Feed
+  // 2. Search
+  // 3. Create Report
+  // 4. View Report
+  // 5. API
   if (index === 1) {
-    return <PageLayout setIndex={setIndex}>Feed....</PageLayout>;
+    return <PageLayout setIndex={setIndex}>Feed..</PageLayout>;
   } else if (index === 2) {
     return <PageLayout setIndex={setIndex}>search....</PageLayout>;
   } else if (index === 3) {
-    return <PageLayout setIndex={setIndex}>create report....</PageLayout>;
+    return <CreateReport setIndex={setIndex} />;
   } else if (index === 4) {
     return <PageLayout setIndex={setIndex}>view report ....</PageLayout>;
   } else if (index === 5) {
@@ -77,5 +94,59 @@ export default function Dashboard() {
         </SidebarInset>
       </SidebarProvider>
     </div>
+  );
+}
+
+function CreateReport({ setIndex }: { setIndex: (index: number) => void }) {
+  return (
+    <PageLayout setIndex={setIndex}>
+      <CardHeader>
+        <CardTitle>Create a Report</CardTitle>
+        <CardDescription>
+          <Popover />
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="Title of Report">Title of Report</Label>
+              <Input
+                id="report"
+                type="report"
+                placeholder="eg - Corruption: Bribe demanded for hospital admission"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="description">Description</Label>
+              </div>
+              <InputGroup>
+                <InputGroupTextarea
+                  className="h-50"
+                  placeholder="A staff member asked for extra money to approve a patient’s admission despite available beds."
+                />
+              </InputGroup>
+            </div>
+          </div>
+        </form>
+      </CardContent>
+    </PageLayout>
+  );
+}
+
+function Popover() {
+  return (
+    <>
+      <div>
+        Corruption: Bribery, misuse of funds, abuse of power Fraud: Financial
+        fraud, fake billing, scams Misconduct: Ethical or professional
+        misconduct Abuse: Abuse of authority or position Negligence:
+        Carelessness causing harm Cover-Up: Evidence suppression or hiding facts
+        Data Tampering: Record manipulation Conflict of Interest: Personal gain
+        over duty Policy Violation: Breaking internal rules
+      </div>
+    </>
   );
 }

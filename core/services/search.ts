fa002ccefff0e03e5aae8d300700f3a7d2da7cec
Anxhul10 <anxhul10@gmail.com>
2025-12-14 21:00:27 +0530

    chore: add search sdk

--- core/services/search.ts
+++ core/services/search.ts
@@ -3,22 +3,16 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl: string = process.env.supabaseUrl || "no key";
 const supabaseKey: string = process.env.supabaseKey || "no key";
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export async function submitReport(
-  user_id: string,
-  title: string,
-  description: string,
-) {
+export async function search(query: string) {
   try {
-    const { data } = await supabase
+    const { data, error } = await supabase
       .from("reports")
-      .insert({ title, description, created_by: user_id })
-      .select();
-    if (data) {
-      return { status: 200 };
-    }
-    return { status: 400 };
+      .select()
+      .ilike("title", query);
+    return data;
   } catch (error) {
     console.log(error);
   }

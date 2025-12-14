a7c35965c37d3b2422d43455724a7e3b4e5dbd8a
Anxhul10 <anxhul10@gmail.com>
2025-12-14 23:16:35 +0530

    chore: add api handler for search

--- core/services/search.ts
+++ core/services/search.ts
@@ -1,19 +1,14 @@
 import "dotenv/config";
 import { createClient } from "@supabase/supabase-js";
+import { ISearch } from "@/types/Search";
 
 const supabaseUrl: string = process.env.supabaseUrl || "no key";
 const supabaseKey: string = process.env.supabaseKey || "no key";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
-interface ISearch {
-  id: number;
-  inserted_at: string;
-  title: string;
-  created_by: string;
-  description: string;
-}
 
-export async function search(query: `%${string}%`): Promise<ISearch[]> {
+// query : `%${string}%`
+export async function search(query: string): Promise<ISearch[]> {
   const { data, error } = await supabase
     .from("reports")
     .select()

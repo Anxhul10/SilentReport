b39470fbbd3bcb9a0b8a293cc18dc628d1c978c6
Anxhul10 <anxhul10@gmail.com>
2025-12-16 01:37:23 +0530

    fix: search alignments

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,10 +5,7 @@ import {
   InputGroupAddon,
   InputGroupInput,
 } from "@/components/ui/input-group";
-import ViewReportContainer from "@/pages/_components/ViewReportContainer";
 import { type IRecordArray } from "@/types/Record";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { Card, CardTitle, CardDescription } from "@/components/ui/card";
 
 import { useEffect, useState } from "react";
@@ -70,7 +67,7 @@ export function SearchBar() {
           <div className="flex justify-center">
             <Card className="w-full h-auto">
               <CardTitle className="ml-5">{val.title}</CardTitle>
-              <CardDescription className="flex justify-end mr-4">
+              <CardDescription className="flex justify-end mr-4 ml-4">
                 created at - {val.inserted_at}
               </CardDescription>
             </Card>
@@ -81,48 +78,3 @@ export function SearchBar() {
   );
 }
 
-// export default function ISearch() {
-//   const [inputStr, setInputStr] = useState("");
-//   const [searchData, setSearchData] = useState([]);
-//   useEffect(() => {
-//     if (!inputStr) return;
-//     fetch("/api/search", {
-//       method: "POST",
-//       body: JSON.stringify({ query: inputStr }),
-//       headers: {
-//         "Content-type": "application/json; charset=UTF-8",
-//       },
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setSearchData(data);
-//       });
-//   }, [inputStr]);
-
-//   return (
-//     <>
-//       <div className="flex ">
-//         <div className="w-full ml-11">
-//           <Input
-//             placeholder="type here..."
-//             onChange={(e) => {
-//               setInputStr(e.target.value);
-//             }}
-//           />
-//         </div>
-//         <div className="ml-10 mr-11">
-//           <Button>Search</Button>
-//         </div>
-//       </div>
-//       {searchData.map((val: IRecordArray) => {
-//         return (
-//           <ViewReportContainer
-//             key={val.id}
-//             title={val.title}
-//             created_at={val.inserted_at}
-//           ></ViewReportContainer>
-//         );
-//       })}
-//     </>
-//   );
-// }

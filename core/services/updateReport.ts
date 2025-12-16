a5f682f9d64624ef3c15b7f0c8c294007716ccd3
Anxhul10 <anxhul10@gmail.com>
2025-12-16 22:48:18 +0530

    chore: add api to delete records

---
+++ core/services/updateReport.ts
@@ -0,0 +1,26 @@
+import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl: string = process.env.supabaseUrl || "no key";
+const supabaseKey: string = process.env.supabaseKey || "no key";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function submitReport(
+  report_id: string,
+  title: string,
+  description: string,
+  visibility: string,
+) {
+  try {
+    const { error } = await supabase
+      .from("reports")
+      .update({ title, visibility, description })
+      .eq("id", report_id);
+    if (error === null) {
+      return { status: 200 };
+    }
+    return { status: 400 };
+  } catch (error) {
+    console.log(error);
+  }
+}

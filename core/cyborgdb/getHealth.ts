3a9ce76842f59a75f833c79846d756293429dad9
Anxhul10 <anxhul10@gmail.com>
2025-12-19 23:16:27 +0530

    chore: add health checks

--- core/cyborgdb/getHealth.ts
+++ core/cyborgdb/getHealth.ts
@@ -1,12 +1,15 @@
+import "dotenv/config";
 import { Client, type HealthResponse } from "cyborgdb";
 
-export async function getHealth(apiKey: string) {
-  const client = new Client({ baseUrl: "http://localhost:8000", apiKey });
+const client = new Client({
+  baseUrl: process.env.baseURL ?? "",
+  apiKey: process.env.CYBORGDB_API_KEY,
+});
 
+export async function getHealth(): Promise<HealthResponse | undefined> {
   try {
     const health: HealthResponse = await client.getHealth();
-    console.log("Service health status:", health);
-    console.log("Status:", health.status);
+    return health;
     // Typical output: { status: 'healthy', api_version: 'v1', version: '1.2.3' }
   } catch (error) {
     console.error("Health check failed:", error);
